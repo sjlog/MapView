3b23f22011eec0b7f8882fd2d96764d4707f2069
sangjo_itwill <sangjo_itwill@local.local>
2013-12-03 10:55:26 +0900

    Initial Commit

---
+++ MapViewTests/MapViewTests.m
@@ -0,0 +1,34 @@
+//
+//  MapViewTests.m
+//  MapViewTests
+//
+//  Created by sangjo_itwill on 2013. 12. 3..
+//  Copyright (c) 2013년 sj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapViewTests : XCTestCase
+
+@end
+
+@implementation MapViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
